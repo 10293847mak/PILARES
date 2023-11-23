680e2eb5bd93edc942718efce013d2590d35068a
Alejandro Coy <calejandrov16.extra@gmail.com>
2023-11-23 15:52:44 -0600

    Casa

--- JAVA/ENTREGABLES/ENTREGABLE 5/src/DiaDeLaSemana.java
+++ JAVA/ENTREGABLES/ENTREGABLE 5/src/DiaDeLaSemana.java
@@ -2,17 +2,12 @@ import java.util.Scanner;
 
 public class DiaDeLaSemana {
     public static void main(String[] args) {
-        // Importa la clase Scanner para la lectura de datos
         Scanner semana = new Scanner(System.in);
-
-        // Crea una variable dia de tipo entero
         int dia;
 
-        // Imprime el texto y hace la lectura del dato desde teclado
         System.out.println("Introduce un número del 1 al 7 para conocer el día de la semana:");
         dia = semana.nextInt();
 
-        // Crea una estructura switch-case
         switch(dia) {
             case 1:
                 System.out.println("Lunes");
@@ -36,10 +31,9 @@ public class DiaDeLaSemana {
                 System.out.println("Domingo");
                 break;
             default:
-                System.out.println("Número no válido");
+                System.out.println("Número no válido ");
                 break;
         }
-        // Cierra el objeto Scanner
         semana.close();
     }
 }
