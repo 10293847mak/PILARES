bb370777801e872f402de05180afeacd022c48e0
Alejandro Coy <calejandrov16.extra@gmail.com>
2023-11-23 13:31:07 -0600

    Casa

---
+++ JAVA/ENTREGABLES/ENTREGABLE 3/App/src/CuentaBancaria.java
@@ -0,0 +1,21 @@
+public class CuentaBancaria {    public static void main(String[] args){
+
+    double saldo = 1000.75;
+
+    double cantidadARetirar= 250;
+
+    double saldoActualizado = saldo - cantidadARetirar;
+    
+    double cantidadParaCadaAmigo = saldoActualizado / 3;
+
+    boolean puedeComprarTicket = false;
+
+    if (cantidadARetirar >= 250) {
+          puedeComprarTicket = true;
+          System.out.println(puedeComprarTicket);
+    }
+    
+      System.out.println("Le di a cada amigo " + cantidadParaCadaAmigo);
+
+    }
+}
--- JAVA/ENTREGABLES/ENTREGABLE 3/CuentaBancaria/src/CuentaBancaria.java
+++ JAVA/ENTREGABLES/ENTREGABLE 3/CuentaBancaria/src/CuentaBancaria.java
@@ -1,4 +1,4 @@
-public class App {
+public class CuentaBancaria {
     public static void main(String[] args){
 
         double saldo = 1000.75;
