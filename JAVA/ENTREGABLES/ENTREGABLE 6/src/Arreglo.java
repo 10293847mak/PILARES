b24004a688412a18a34ee872dc0c2185b48cd0ff
Alejandro Coy <calejandrov16.extra@gmail.com>
2023-11-23 17:19:38 -0600

    Casa

---
+++ JAVA/ENTREGABLES/ENTREGABLE 6/src/Arreglo.java
@@ -0,0 +1,9 @@
+public class Arreglo {
+    public static void main(String[] args)  {
+        int[] numeros = {12,10,4,25,5,5,9};
+
+        for(int i = 0; i < numeros.length; i++){
+            System.out.println("El arreglo en la posición " + i + "tiene el número " + numeros[i]);
+        }
+    }
+}
